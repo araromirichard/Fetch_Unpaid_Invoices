f76e5f367f6338fd68d3738a68127289f9929399
karosaxy <araromirichard@gmail.com>
2021-12-13 14:09:32 +0100

    initial commit

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/karosaxy/paystack-client/pkg/client/paystack"
 )
 
-
 const SECRET_KEY = "sk_test_3cec88e7ece6d9f5b69f33c013b94cc9142bf161"
 const BASE_URL = "https://api.paystack.co"
 
@@ -16,14 +15,12 @@ func main() {
 	secretKey := os.Getenv("PAYSTACK_SECRET_KEY")
 	client := paystack.NewClient(BASE_URL, secretKey)
 
-
-	ccq := paystack.CreateCustomerRequest{"man@go.com" }
-
+	ccq := paystack.CreateCustomerRequest{"man@go.com"}
 
 	if err := client.CreateCustomer(ccq); err != nil {
 		log.Fatalf("could not create customer. %v", err)
 	}
 
 	log.Println("Customer successfully created")
-	
+
 }
